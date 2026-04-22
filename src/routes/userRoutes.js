97bcc8c82d766b25d844bbc71d820bdfd73cf551
taitami <vladm2329@gmail.com>
2026-04-22 18:10:52 +0300

    chore: rm

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,12 +7,10 @@ const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
-// Initialize handlers
 const registerHandler = new RegisterUserHandler();
 const loginHandler = new LoginUserHandler();
 const getProfileHandler = new GetProfileHandler();
 
-// Initialize controller with handlers
 const userController = new UserController(registerHandler, loginHandler, getProfileHandler);
 
 router.post("/register", userController.register);
