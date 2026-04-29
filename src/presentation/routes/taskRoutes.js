0b6dd6fce37d3b2acb133f191da00f889ffdf9b3
Edward <edwardkawa907@gmail.com>
2026-04-29 14:42:51 +0300

    fix(routes): resolve merge conflict and remove unused TaskService import

--- src/presentation/routes/taskRoutes.js
+++ src/presentation/routes/taskRoutes.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const TaskController = require("../controllers/taskController");
-const TaskService = require("../../application/services/taskService");
 const taskRepository = require("../../infrastructure/repositories/taskRepository");
 const authMiddleware = require("../middleware/auth");
 const eventBus = require("../../infrastructure/events/eventBus");
 
+const CreateTaskHandler = require("../../application/commands/task/create-task.handler");
+const UpdateTaskHandler = require("../../application/commands/task/update-task.handler");
+const DeleteTaskHandler = require("../../application/commands/task/delete-task.handler");
+const GetTasksHandler = require("../../application/queries/task/get-tasks.handler");
+const GetTaskByIdHandler = require("../../application/queries/task/get-task-by-id.handler");
+
 const router = express.Router();
 
-const taskController = new TaskController(taskRepository, eventBus);
+const createHandler = new CreateTaskHandler(taskRepository, eventBus);
+const updateHandler = new UpdateTaskHandler(taskRepository, eventBus);
+
+const deleteHandler = new DeleteTaskHandler(taskRepository);
+const getTasksQuery = new GetTasksHandler(taskRepository);
+const getTaskByIdQuery = new GetTaskByIdHandler(taskRepository);
+
+const taskController = new TaskController(
+  createHandler,
+  updateHandler,
+  deleteHandler,
+  getTasksQuery,
+  getTaskByIdQuery,
+);
 
 router.post("/", authMiddleware, taskController.createTask);
 router.get("/", authMiddleware, taskController.getTasks);
